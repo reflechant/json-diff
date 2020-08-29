5a1f51865b52874f0e784da751a02f80f1258b26
Roman Gerasimov <reflechant@users.noreply.github.com>
2020-08-29 03:59:40 +0300

    v.0.1.0

--- src/json_diff/core.clj
+++ src/json_diff/core.clj
@@ -1,15 +1,25 @@
 (ns json-diff.core
+  (:require [clojure.data.json :as json]
+            ;; [clojure.core.async :as async]
+            [lambdaisland.deep-diff2 :as ddiff]
+            [clojure.java.io :as io])
   (:gen-class))
+
 
 (defn diff
   "prints diff between JSON files"
-  [file1 file2])
+  [fn1 fn2]
+  (with-open [file1 (io/reader fn1)
+              file2 (io/reader fn2)]
+    (let [json1 (json/read file1)
+          json2 (json/read file2)]
+      (ddiff/pretty-print (ddiff/diff json1 json2)))))
 
 (defn -main
   "Main function"
   [& args]
   (if (> 2 (count args))
     (println "Not enough parameters!")
-    (let [file1 (first args)
-          file2 (second args)]
-      (diff file1 file2))))
+    (let [fn1 (first args)
+          fn2 (second args)]
+      (diff fn1 fn2))))
