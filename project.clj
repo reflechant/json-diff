c04ed7a1209d2545a2856c3f99fc8837b37b8cfe
Roman Gerasimov <reflechant@users.noreply.github.com>
2020-08-29 21:16:05 +0300

    minor project description change

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject json-diff "0.2.0"
-  :description "A console program to produce colorized diff of two JSON files"
+  :description "A console program to produce colorized structural diff of two JSON files"
   :url "https://github.com/reflechant/json-diff"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
