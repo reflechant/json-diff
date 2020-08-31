6b4c6b2664d7a3dacb7efd6081ed106036acc311
Roman Gerasimov <reflechant@users.noreply.github.com>
2020-08-31 03:07:20 +0300

    added tools.cli to parse command line params

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [lambdaisland/deep-diff2 "2.0.108"]
-                 [org.clojure/data.json "1.0.0"]
                  [org.clojure/core.async "1.3.610"]
+                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/tools.cli "1.0.194"]
+                 [lambdaisland/deep-diff2 "2.0.108"]
                  [juji/editscript "0.4.6"]]
   :main ^:skip-aot json-diff.core
   :target-path "target/%s"
--- src/json_diff/core.clj
+++ src/json_diff/core.clj
@@ -2,31 +2,54 @@
   (:require [clojure.data.json :as json]
             [clojure.core.async :refer [>! <!! go chan]]
             [lambdaisland.deep-diff2 :as ddiff]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [editscript.core :as edcore]
+            [editscript.edit :as edit]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
+(def cli-options
+  [["-m" "--mode MODE" "operation mode: 'visual' or 'patch'"
+    :default :patch
+    :parse-fn #(keyword %)
+    :validate [#(% #{:visual :patch}) "possible modes are 'visual' or 'patch'"]]])
 
-(defn open-parse
+(defn parse-file
   "open JSON file and parses it"
   [fn]
   (with-open [f (io/reader fn)]
     (json/read f)))
 
+(defn parse-files
+  "parses multiple file asynchronously"
+  [& files]
+  ())
+
+(defn patch
+  "returns compact patch between objects"
+  [json1 json2]
+  )
+
 (defn diff
-  "returns diff between JSON files"
+  "returns visual diff between JSON files"
   [fn1 fn2]
   (let [c1 (chan)
         c2 (chan)]
-    (go (>! c1 (open-parse fn1)))
-    (go (>! c2 (open-parse fn2)))
-    (ddiff/diff (<!! c1) (<!! c2))))
+    (go (>! c1 (parse-file fn1)))
+    (go (>! c2 (parse-file fn2)))
+    (ddiff/pretty-print
+     (ddiff/diff (<!! c1) (<!! c2)))))
 
 (defn -main
   "main function"
   [& args]
-  (if (> 2 (count args))
-    (println "Not enough parameters!")
-    (ddiff/pretty-print
-     (diff
-      (first args)
-      (second args)))))
+  (let [{files :arguments
+         errors :errors
+         {:keys [mode]} :options} (parse-opts args cli-options)]
+    (cond
+      errors (do (apply println errors)
+                 (System/exit 1))
+      (not= 2 (count files)) (do (println "you need to pass two JSON files!")
+                                   (System/exit 1))
+      (= :visual mode) (apply diff files)
+      (= :patch mode) (apply patch files))))
