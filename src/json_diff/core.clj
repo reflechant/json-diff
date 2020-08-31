28b77154ce37891f64d602624ca364810d4eb641
Roman Gerasimov <reflechant@users.noreply.github.com>
2020-08-31 03:36:21 +0300

    refactoring

--- src/json_diff/core.clj
+++ src/json_diff/core.clj
@@ -4,7 +4,7 @@
             [lambdaisland.deep-diff2 :as ddiff]
             [clojure.java.io :as io]
             [editscript.core :as edcore]
-            [editscript.edit :as edit]
+            ;; [editscript.edit :as edit]
             [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
@@ -14,31 +14,34 @@
     :parse-fn #(keyword %)
     :validate [#(% #{:visual :patch}) "possible modes are 'visual' or 'patch'"]]])
 
-(defn parse-file
-  "open JSON file and parses it"
-  [fn]
-  (with-open [f (io/reader fn)]
+(defn parse
+  "opens JSON file and parses it"
+  [file-name]
+  (with-open [f (io/reader file-name)]
     (json/read f)))
 
-(defn parse-files
-  "parses multiple file asynchronously"
-  [& files]
-  ())
+(defn parse-async
+  "opens JSON file and parses it asynchronously, returns channel"
+  [file-name]
+  (go (parse file-name)))
+
+(defn process
+  [f & files]
+  (->> files
+       (map parse-async)
+       (map #(<!! %))
+       (map f)))
 
 (defn patch
-  "returns compact patch between objects"
-  [json1 json2]
-  )
+  "returns compact patch between two JSON objects"
+  [left right]
+  (println (edcore/diff left right {:algo :quick})))
 
 (defn diff
-  "returns visual diff between JSON files"
-  [fn1 fn2]
-  (let [c1 (chan)
-        c2 (chan)]
-    (go (>! c1 (parse-file fn1)))
-    (go (>! c2 (parse-file fn2)))
-    (ddiff/pretty-print
-     (ddiff/diff (<!! c1) (<!! c2)))))
+  "returns visual diff between two JSON objects"
+  [left right]
+  (ddiff/pretty-print
+   (ddiff/diff left right)))
 
 (defn -main
   "main function"
@@ -51,5 +54,5 @@
                  (System/exit 1))
       (not= 2 (count files)) (do (println "you need to pass two JSON files!")
                                    (System/exit 1))
-      (= :visual mode) (apply diff files)
-      (= :patch mode) (apply patch files))))
+      (= :visual mode) (apply process diff files)
+      (= :patch mode) (apply process patch files))))
